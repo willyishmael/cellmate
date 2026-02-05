c173ce365e31d8c54cc899b3288ce9c7bceb001d
William Yishmael <wtangka22@gmail.com>
2026-02-05 11:34:59 +0900

    Update version.py

--- model/version.py
+++ model/version.py
@@ -1,7 +1,7 @@
 import os
 
 # Centralized application version. Override by setting APP_VERSION at runtime/build time.
-__version__ = os.environ.get("APP_VERSION", "1.0.0")
+__version__ = os.environ.get("APP_VERSION", "1.0.2")
 
 
 def get_version() -> str:
